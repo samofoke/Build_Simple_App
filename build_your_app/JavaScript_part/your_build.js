5f4da8729a8ff501f6eef1d858151c23b823092a
samofoke <samofoke@student.wethinkcode.co.za>
2021-10-05 13:06:35 +0200

    just added the JavaScript

---
+++ build_your_app/JavaScript_part/your_build.js
@@ -0,0 +1,27 @@
+let header = document.querySelector('.header');
+let site = document.querySelector('.site');
+let more = document.querySelector('.more');
+
+
+let imageOverlay = document.querySelector('.imageContainer').children[0];//we selecting on the first child element
+let mainImage = document.querySelector('.imageContainer').children[1];
+
+let links = Array.from(document.querySelectorAll('.link'));
+
+
+function animate() {
+    setTimeout(() => {
+        header.classList.add('active');
+        imageOverlay.style.animation = 'slide 1s forwards';
+        mainImage.style.animation = 'slide-perm 1s forwards';
+    }, 2000);
+
+    setTimeout(() => {
+        site.classList.add('active');
+        more.classList.add('active');
+        
+    }, 3000);
+}
+
+animate()
+
